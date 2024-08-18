4c585a0e95466c10e0f751c12d4dd7d3205c7c81
Thienthanh Trinh <75050493+ttrinh2306@users.noreply.github.com>
2024-08-18 00:36:12 -0400

    updated pynb to train and save models

---
+++ code/required_packages.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+from time import sleep
+from pathlib import Path
+
+from fastdownload import download_url
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
+from tensorflow.keras.optimizers import Adam, SGD
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
+
+import matplotlib.pyplot as plt
+import random
+from PIL import Image
+
+import shutil
+import pickle
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_curve, auc
+import seaborn as sns
+
+from functions import *
+
+import yaml
+import os
+
+import time
